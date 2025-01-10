d39b8155ad51dfa07eda1b350c9f73c3ec306b40
mehmonov <uzbekfreedom@gmail.com>
2025-01-10 16:26:40 +0500

    remove other directories

--- docs/lib/routes-config.ts
+++ docs/lib/routes-config.ts
@@ -23,23 +23,7 @@ export const ROUTES: EachRoute[] = [
         title: "Project Structure",
         href: "/project-structure",
       },
-      {
-        title: "Components",
-        href: "/components",
-        items: [
-          { title: "Stepper", href: "/stepper" },
-          { title: "Tabs", href: "/tabs" },
-          { title: "Note", href: "/note" },
-          { title: "Code Block", href: "/code-block" },
-          { title: "Image & Link", href: "/image-link" },
-          { title: "Custom", href: "/custom" },
-        ],
-      },
-      { title: "Themes", href: "/themes" },
-      {
-        title: "Customize",
-        href: "/customize",
-      },
+      
     ],
   },
 ];
